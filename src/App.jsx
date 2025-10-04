e247bc0a12fca2ba2da89ccfafca6085380a770b
suhas152 <vedasuhaskulkurni@gmail.com>
2025-10-04 14:25:15 +0530

    V3.0

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>VED+SUHAS+SRESTA</h1>
+      <h1>VED+SUHAS+SRESTA+KULKARNI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is:- {count}
